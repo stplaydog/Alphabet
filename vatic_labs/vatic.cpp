e1963808b51750944a0c34702861c49547655985
Ubuntu <ubuntu@ip-172-31-7-235.us-west-2.compute.internal>
2016-09-20 17:55:30 +0000

    working version

--- vatic_labs/vatic.cpp
+++ vatic_labs/vatic.cpp
@@ -1,19 +1,20 @@
-#include <iostream>
 #include "CSVparser.h"
+#include <iostream>
 #include <map>
 #include <vector>
 #include <algorithm>
 #include <limits>
 #include <cassert>
 #include <queue>
+#include <iomanip>
 
 using namespace std;
 
 #define EPS 0.001
 
-bool same(double a, double b) {
-    bool ret = fabs(a - b) <= EPS; 
-    return ret;
+bool same(double a, double b) 
+{
+    return fabs(a - b) <= EPS; 
 }
 
 
@@ -74,12 +75,16 @@ public:
 
         for(auto i = m_index.begin(); i != m_index.end(); ++i)
             sort(i->second.begin(), i->second.end());
+        //dump();
     }
 
     Item searchLatest(int time, string symb)
     {
         auto pos = lower_bound(m_index[symb].begin(), m_index[symb].end(), time);
-        vector<Item> d = m_data[pos-m_index[symb].begin()];
+        int t    = pos-m_index[symb].begin();
+        if(t==m_index[symb].size())
+            t = m_index[symb][m_index[symb].size()-1];
+        vector<Item> d = m_data[t];
         for(auto it = d.begin(); it != d.end(); ++it)
         {
             if(it->symb == symb)
@@ -123,16 +128,18 @@ public:
         string symb;
         double price;
         int    quantity;
+        string side;
 
         void dump()
         {
-            cout<<symb.c_str()<<" "<<time<<" "<<price<<" "<<quantity<<endl;
+            cout<<symb.c_str()<<" "<<side<<" "<<time<<" "<<price<<" "<<quantity<<endl;
         }
     };
 
     typedef map<string, queue<Item>> inventory;
 
-    TradeData(const char* file)
+    TradeData(const char* priceFile, const char *file)
+        : m_data(priceFile)
     {
         csv::Parser f = csv::Parser(file);
         for(uint32_t i=0; i<f.rowCount(); i++)
@@ -141,46 +148,53 @@ public:
             int time      = stoi(f[i][0]);
             itm.time      = time;
             itm.symb      = f[i][1];
+            itm.side      = f[i][2];
             itm.price     = stof(f[i][3]); 
             itm.quantity  = stoi(f[i][4]); 
 
             if(f[i][2] == "B")
             {
-                if(!check_finish(itm))
+                if(!check_finish(itm, m_pend))
                     place_item(itm, m_inv);
             }
             else
             {
-                if(!check_finish(itm))
+                if(!check_finish(itm, m_inv))
                     place_item(itm, m_pend);
             }
+            //dump();
         }
     }
 
     bool check_finish(Item & itm, inventory & inv)
     {
         auto it = inv.find(itm.symb);
-        if(!it)
+        if(it == inv.end())
             return false;
         else
         {
-            while(itm.quantity && it != inv[itm.symb].end())
+            queue<Item> &q = inv[itm.symb];
+            while(itm.quantity && !q.empty())
             {
-                int diff = itm.quantity - it->quantity;
-                print_record(itm, *it);
+                Item &elem = q.front();
+                int diff = itm.quantity - elem.quantity;
+                print_record(elem, itm);
                 if(diff == 0)
                 {
-                    erase(it++);
+                    q.pop();
+                    itm.quantity    = 0;
                     break;
                 }
                 else if(diff < 0)
                 {
-                    it->quantity -= itm.quantity;
+                    elem.quantity  -= itm.quantity;
+                    itm.quantity    = 0;
                     break;
                 }
                 else if(diff > 0)
                 {
-                    erase(it++);
+                    itm.quantity    = diff; 
+                    q.pop();
                 }
             }
             if(itm.quantity)
@@ -191,17 +205,78 @@ public:
 
     void print_record(Item & a, Item & b)
     {
+        int ot      = a.time;
+        int ct      = b.time;
+        string symb = a.symb;
+        int qt      = a.quantity>b.quantity ? b.quantity : a.quantity;
+        double PNL  = calPNL(a, b, qt);
+        string os   = a.side;
+        string cs   = b.side;
+        double op   = a.price;
+        double cp   = b.price;
+
+        PriceData::Item oit = m_data.searchLatest(a.time, a.symb);
+        double ob   = oit.bid; 
+        double oa   = oit.ask; 
+
+        PriceData::Item cit = m_data.searchLatest(b.time, b.symb);
+        double cb   = cit.bid; 
+        double ca   = cit.ask; 
+
+        string ol   = getLiquidity(a.side, op, ob, oa);
+        string cl   = getLiquidity(b.side, cp, cb, ca);
+
+
+        cout<<fixed;
+        cout<<setprecision(2);;
+        cout<<ot<<","<<ct <<","<<symb<<","
+            <<qt<<","<<PNL<<","
+            <<os<<","<<cs <<","
+            <<op<<","<<cp <<","
+            <<ob<<","<<cb <<","
+            <<oa<<","<<ca <<","
+            <<ol<<","<<cl 
+            <<endl;
+    }
+
+    string getLiquidity(string &type, double price, double bid, double ask)
+    {
+        if(type == "B")
+        {
+            return price <= bid ? "P" : "A";
+        }
+        else
+        {
+            return price >= ask ? "P" : "A";
+        }
+    }
+
+    double calPNL(Item & a, Item & b, int qt)
+    {
+        double buy;
+        double sell;
+        if(a.side == "B")
+        {
+            buy  = a.price;
+            sell = b.price;
+        }
+        else
+        {
+            buy  = b.price;
+            sell = a.price;
+        }
+        return (sell- buy)*qt;
     }
 
 
     void place_item(const Item & itm, inventory & inv)
     {
         auto it = inv.find(itm.symb);
-        if(it == m_inv.end())
+        if(it == inv.end())
         {
             queue<Item> q;
             q.push(itm);
-            m_inv[itm.symb] = q;
+            inv[itm.symb] = q;
         }
         else
             it->second.push(itm);
@@ -209,20 +284,46 @@ public:
 
     void dump()
     {
+        cout<<"inventory:"<<endl;
+        for(auto it = m_inv.begin(); it != m_inv.end(); ++it)
+        {
+            for(int i=0; i<it->second.size(); i++)
+            {
+                Item itm = it->second.front();
+                it->second.pop();
+                itm.dump();
+                it->second.push(itm);
+            }
+        }
+
+        cout<<"buying:"<<endl;
+        for(auto it = m_pend.begin(); it != m_pend.end(); ++it)
+        {
+            for(int i=0; i<it->second.size(); i++)
+            {
+                Item itm = it->second.front();
+                it->second.pop();
+                itm.dump();
+                it->second.push(itm);
+            }
+        }
+
+        cout<<"====="<<endl;
     }
 
     inventory m_inv;
     inventory m_pend;
+    PriceData m_data;
 };
 
 
 int main(int argc, char* argv[])
 {
-    PriceData p(argv[1]);    
+    TradeData t(argv[1], argv[2]);
 
-    PriceData::Item item = p.searchLatest(2, string("ABC"));
-    assert(item.time == 1 && true == same(item.bid, 10.05) && same(item.ask, 10.06) && item.symb == "ABC");
+    //TradeData t1("quo.csv", "trd.csv");
+    //PriceData::Item item = t1.m_data.searchLatest(2, string("ABC"));
+    //assert(item.time == 1 && true == same(item.bid, 10.05) && same(item.ask, 10.06) && item.symb == "ABC");
 
-    TradeData t(argv[2]);
     return 0;
 }
