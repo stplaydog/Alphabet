c7f895005fafb3ee1987845bab3c01bc5688bb78
Zhaoming Yin <zhaomingyin@google.com>
2020-03-12 13:11:00 -0700

    remote flash station

---
+++ flash/flash.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# This is a comment!
+
+./remote_local_flashstation \
+    --android_tree zhaomingyin@zhaoming.mtv.corp.google.com:/usr/local/google/home/zhaomingyin/second_drive/ag/ \
+    -t crosshatch_car_permissive-userdebug
